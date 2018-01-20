045dd4a264f5c9fd0b351dbc2a9134e7eb58e9f5
fayeoyaee <ouyang.f@husky.neu.edu>
2018-01-20 09:21:22 -0800

    random

--- public/router.js
+++ public/router.js
@@ -3,6 +3,7 @@ angular.module("userApp", ["ngRoute"])
     $routeProvider
     .when("/home", {
         templateUrl : "home.html",
+        controller : "mainController"
     })
     .when("/register", {
         templateUrl : "register.html",
@@ -16,10 +17,16 @@ angular.module("userApp", ["ngRoute"])
         templateUrl : "logout.html",
         controller: "logoutController"
     })
+    .when("/profile", {
+        templateUrl : "profile.html",
+        controller: "profileController"
+    })
     .when("/profile/:userId", {
         templateUrl : "profile.html",
         controller: "profileController"
     });
+})
+.controller("mainController", function($scope, cookieService){
 })
 .controller("registerController", function($scope, $http, $location){
     $scope.register = function register(){
@@ -32,33 +39,61 @@ angular.module("userApp", ["ngRoute"])
         })
     };
 })
-.controller("loginController", function($scope, $http, $location, $rootScope){
+.controller("loginController", function($scope, $http, $location){
     $scope.login = function login(){
         $http.post("/login", $scope.user)
         .then(function successCallback(res){
+            document.cookie = "username="+res.data.user.username;
+            document.cookie = "userid="+res.data.user._id;
             console.log(res.data.message);
-            $rootScope.currentUser = true;
             $location.url("/profile/" + res.data.user._id);
         }, function errorCallback(res){
             window.alert(res.data.message)
-            location.reload();
         })
     }
 })
-.controller("logoutController", function($timeout, $location, $rootScope){
+.controller("logoutController", function($timeout, $location){
     $timeout(function() {
-        $rootScope.currentUser = false;
-        $location.path('/home');
+        document.cookie = "username=";
+        document.cookie = "userid=";
+        $location.url('/home');
     }, 3000);
 
 })
-.controller("profileController", function($scope, $http, $rootScope, $routeParams){
-    if ($rootScope.currentUser) {
-        $http.get("/profile/"+$routeParams.userId)
+.controller("profileController", function($scope, $http, $routeParams, $location, cookieService){
+    if (cookieService.getCookie("username")) {
+        if (!$routeParams.userId) {
+            $location.url("/profile/" + cookieService.getCookie("userid"))
+        }
+
+        $http.get("/profile/"+cookieService.getCookie("userid"))
         .then(function successCallback(res){
             $scope.user = res.data.user 
         }, function errorCallback(res){
             console.log(res.data.message)
         })
     }
-});
+})
+.factory('cookieService', function () {
+    var service = {
+        getCookie : getCookie
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+ 
+    return service;
+  })
