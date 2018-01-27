bee6aa93702309c13d41cb062970f06e6649a6ac
fayeoyaee <ouyang.f@husky.neu.edu>
2018-01-26 17:21:52 -0800

    using redux-localstorage to persist redux state

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { compose, createStore } from 'redux'
+import persistState from 'redux-localstorage'
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import bfcApp from "./reducers";
 
+const enhancer = compose(
+    persistState(),
+)
+
 ReactDOM.render((
-    <Provider store={createStore(bfcApp)}>
+    <Provider store={createStore(bfcApp, enhancer)}>
         <CookiesProvider>
             <BrowserRouter>
                 <App />
