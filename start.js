56b0f5e6ba3282a3f081cf2fd2c11081ecb97e3b
fayeoyaee <ouyang.f@husky.neu.edu>
2018-02-08 02:24:33 -0800

    with react

--- start.js
+++ start.js
@@ -1,12 +1,17 @@
 // start express server and mongodb
 const express = require("express");
 const mongoClient = require("mongodb").MongoClient;
+const path = require('path');
 
 const server = express();
+// Serve static files from the React after it is built
+server.use(express.static(path.join(__dirname, 'client/build')));
+// Listen to PORT defined in .env
 server.listen(process.env.PORT, function() {
     console.log("app listening");
 })
 
+// Connect to URI defined in .env
 mongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     if (err) {
         console.log("Error: unable to connect to db");
