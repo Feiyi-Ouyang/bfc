1aae945cc589ee55fffd8d31928dc06c95197c5c
fayeoyaee <ouyang.f@husky.neu.edu>
2018-03-07 17:57:54 -0800

    modulize db models

---
+++ db/model/product.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose');
+
+var productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('product', productSchema);
+
---
+++ db/model/user.js
@@ -0,0 +1,23 @@
+let mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    password_v: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('user', userSchema);
+
